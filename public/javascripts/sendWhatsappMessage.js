edd46e4ddbe2681645f276f9843128cafa4f18bd
biko2020 <brahimlion38@hotmail.com>
2022-08-29 17:18:52 +0100

    backend: rectifier le code du ficheir sendDevis

--- public/javascripts/sendWhatsappMessage.js
+++ public/javascripts/sendWhatsappMessage.js
@@ -1,28 +1,31 @@
-const { Client } = require("whatsapp-web.js");
-const qrcode = require("qrcode-terminal");
+const sendToWhatsApp = () => {
 
-const client = new Client({
-   puppeteer: {
-     args: ["--disable-setuid-sandbox"],
-   },
-});
-  client.on("qr", (qr) => {
-    //console.log('QR RECEIVED', qr);
-    qrcode.generate(qr, { small: true });
+  const { Client } = require("whatsapp-web.js");
+  const qrcode = require("qrcode-terminal");
+  
+  const client = new Client({
+     puppeteer: {
+       args: ["--disable-setuid-sandbox"],
+     },
   });
-
-  client.on("ready", () => {
-    console.log("Client is ready!");
-  });
-  client.on("message", (message) => {
-    console.log(message.body);
-  });
-  client.on("message", (message) => {
-    if (message.body === "!ping") {
-      message.reply("pong");
-    }
-  });
-
-  client.initialize();
+    client.on("qr", (qr) => {
+      //console.log('QR RECEIVED', qr);
+      qrcode.generate(qr, { small: true });
+    });
+  
+    client.on("ready", () => {
+      console.log("Client is ready!");
+    });
+    client.on("message", (message) => {
+      console.log(message.body);
+    });
+    client.on("message", (message) => {
+      if (message.body === "!ping") {
+        message.reply("pong");
+      }
+    });
+  
+    client.initialize();
+}
 
 //export {sendToWhatsApp};
